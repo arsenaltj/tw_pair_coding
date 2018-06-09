fc8dc31b35b27310885748fa867f613b8e275afe
arsenaltj <645083398@qq.com>
2018-06-09 12:52:29 +0800

    add method:'change_state()' about: core logic:judge the cell's state

--- life_game.py
+++ life_game.py
@@ -36,5 +36,18 @@ def draw():
                 screen.blit(new_cell.image, new_cell.rect)
 
 
+def change_state():
+    '''核心逻辑：
+            nbrs_count 是计算一个细胞周围细胞的个数
+            然后判断，当周围有3个，有两个 以及其他情况下的状态改变
+    '''
+    nbrs_count = sum(np.roll(np.roll(pygame.world, i, 0), j, 1)
+                     for i in (-1, 0, 1) for j in (-1, 0, 1)
+                     if (i != 0 or j != 0))
+
+    pygame.world = (nbrs_count == 3) | (
+        (pygame.world == 1) & (nbrs_count == 2)).astype('int')
+
+
 if __name__ == "__main__":
     screen = pygame.display.set_mode((WIDTH * Cell.size, HEIGHT * Cell.size))
