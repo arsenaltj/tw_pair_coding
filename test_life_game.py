4df6bc6d4f889b3fa4ed5e2f13fdc3110774ab9a
arsenaltj <645083398@qq.com>
2018-06-09 15:25:53 +0800

    add more method of unittest coreLogic

--- test_life_game.py
+++ test_life_game.py
@@ -1,21 +1,24 @@
+# -*- coding:utf8 -*-
 import unittest
 import life_game as lg
 
 lg.pygame.world = lg.np.zeros((lg.HEIGHT, lg.WIDTH))
 
+'''分别测试了当一个活着的细胞或死去的细胞周围有n个活着细胞的时候 细胞的变化情况'''
+
 
 class TestLifeGame(unittest.TestCase):
+    def test_coreLogic_cell_f1_0(self):
+        self.assertEqual(lg.judge_cell_state(-1)[0][0], 0)
+
+    def test_coreLogic_cell_f1_1(self):
+        lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
+        self.assertEqual(lg.judge_cell_state(-1)[0][0], 0)
+
     def test_coreLogic_cell0_0(self):
         self.assertEqual(lg.judge_cell_state(0)[0][0], 0)
 
     def test_coreLogic_cell0_1(self):
-        lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
-        self.assertEqual(lg.judge_cell_state(0)[0][0], 0)
-
-    def test_coreLogic_cell1_0(self):
-        self.assertEqual(lg.judge_cell_state(1)[0][0], 0)
-
-    def test_coreLogic_cell1_1(self):
         lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
         self.assertEqual(lg.judge_cell_state(0)[0][0], 0)
 
@@ -33,16 +36,16 @@ class TestLifeGame(unittest.TestCase):
         lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
         self.assertEqual(lg.judge_cell_state(3)[0][0], 1)
 
-    def test_coreLogic_cell4_0(self):
-        self.assertEqual(lg.judge_cell_state(4)[0][0], 0)
-
-    def test_coreLogic_cell4_1(self):
-        lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
-        self.assertEqual(lg.judge_cell_state(4)[0][0], 0)
-
     def test_coreLogic_cell8_0(self):
         self.assertEqual(lg.judge_cell_state(8)[0][0], 0)
 
     def test_coreLogic_cell8_1(self):
         lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
         self.assertEqual(lg.judge_cell_state(8)[0][0], 0)
+
+    def test_coreLogic_cell9_0(self):
+        self.assertEqual(lg.judge_cell_state(9)[0][0], 0)
+
+    def test_coreLogic_cell9_1(self):
+        lg.pygame.world = lg.np.ones((lg.HEIGHT, lg.WIDTH))
+        self.assertEqual(lg.judge_cell_state(9)[0][0], 0)
